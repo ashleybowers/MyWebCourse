05de4737b23d4a2c7d3a36381f4a9d8467e77833
Ashley Bowers <ashleymariebowers@gmail.com>
2019-03-14 10:02:35 -0400

    Changes for labs.

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+let x, y, z;
+x = 10;
+y = '10';
+z = 30;
+
+console.log(`x is ${typeof x}`);
+console.log(`y is ${typeof y}`);
+console.log(`z is ${typeof z}`);
+
+var newX = x++;
+
+console.log(`newX is ${newX}`);
+console.log(`x == y is ${x == y}`);
+
+let timeInMs = Date.now();
+console.log(`${timeInMs}`);
